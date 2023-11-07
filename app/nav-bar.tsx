50dce688b93a03826779be2c67811d83279b7fcd
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-11-07 11:31:41 +0200

    styling active link in nav

--- app/nav-bar.tsx
+++ app/nav-bar.tsx
@@ -1,17 +1,40 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+
 export default function Navbar() {
-    const links: { label: string; link: string }[]  = [
-        {label:'Dashboard', link:'/'},
-        {label:'Issues', link:'/issues'}
-    ]
+  const currentPath = usePathname();
+  const links: { label: string; link: string }[] = [
+    { label: "Dashboard", link: "/" },
+    { label: "Issues", link: "/issues" },
+  ];
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href={`/`}><AiFillBug /></Link>
+      <Link href={`/`} className="text-purple text-4xl text-pink-600">
+        <AiFillBug />{" "}
+      </Link>
       <ul className="flex  space-x-6">
-        {links.map((link) =>  <Link key={link.link} className="text-zinc-500 hover:text-zinc-800 transition-colors" href={`/`}>{link.label}</Link>)} 
+        {links.map((link) => (
+          <Link
+            key={link.link}
+            className={classnames({
+                'text-pink-600': link.link === currentPath,
+                'text-zinc-500': link.link !== currentPath,
+                'hover:text-pink-600 transition-colors font-medium': true
+            })}
+            href={link.link}
+          >
+            {/* ${
+              link.link === currentPath ? "text-pink-600" : "text-zinc-500"
+            } hover:text-pink-600 transition-colors font-medium */}
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
