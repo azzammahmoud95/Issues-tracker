f5131f731c4eafff167e8e35eed6f4ffade859ea
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-11-17 15:11:34 +0200

    get logged user issues grouped by status

--- app/api/profile/issues/route.ts
+++ app/api/profile/issues/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+import { Issue } from "@prisma/client";
+import prisma from "@/prisma/client";
 // Create a server-side function
 export async function GET() {
   // Get the session using the getSession function
@@ -11,59 +13,31 @@ export async function GET() {
       status: 401,
     });
   }
+  const allIssues = await prisma.issue.findMany({
+    where: {
+      assignedToUserId: session.user.userid,
+      status: {
+        in: ["OPEN", "IN_PROGRESS", "CLOSED"],
+      },
+    },
+  });
 
-  // If there is a session, you can access the user information
-  console.log("User ID:", session.user.userid);
-  console.log("User Email:", session.user.email);
-  console.log("User Name:", session.user.name);
+  
+  const issuesByStatus: {
+    OPEN: Issue[];
+    IN_PROGRESS: Issue[];
+    CLOSED: Issue[];
+  } = {
+    OPEN: [],
+    IN_PROGRESS: [],
+    CLOSED: [],
+  };
+  // ==> Filtering status and push them inside the object depending on status
+  allIssues.forEach((issue) => {
+    issuesByStatus[issue.status].push(issue);
+  });
 
-  // Your logic for handling the authenticated user goes here
-
-  // Return a response as needed
-  return new NextResponse(JSON.stringify({ message: "Success" }));
+  return new NextResponse(
+    JSON.stringify({ status: 200, message: issuesByStatus })
+  );
 }
-// import { NextRequest, NextResponse } from "next/server";
-// import prisma from "@/prisma/client";
-// import { getOptions} from "@/app/api/auth/[...nextauth]/route"
-// import { options } from "../../auth/[...nextauth]/options";
-// export async function GET(
-//   req: NextRequest,
-//   { params }: { params: { id: string } }
-// ) {
-//   try {
-//     const session = await getOptions(options);
-//     if (!session) {
-//       return new Response(
-//         JSON.stringify({ message: "Unauthorized" }),
-//         { status: 401 }
-//       );
-//     }
-
-//     const userIssueCounts = await prisma.issue.groupBy({
-//       by: ["status"],
-//       where: {
-//         assignedToUserId: session.user.userid,
-//       },
-//       _count: {
-//         status: true,
-//       },
-//     });
-
-//     const formattedIssueCounts = userIssueCounts.reduce(
-//       (acc, { status, _count }) => {
-//         acc[status] = _count;
-//         return acc;
-//       },
-//       {}
-//     );
-
-//     return new Response(JSON.stringify(formattedIssueCounts), {
-//       status: 200,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return new Response(JSON.stringify(error), { status: 500 });
-//   } finally {
-//     prisma.$disconnect();
-//   }
-// }
