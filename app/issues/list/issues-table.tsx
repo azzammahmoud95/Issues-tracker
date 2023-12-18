1b67dc6a1559ef0c0001f6edc1fc07917ef1d91a
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-12-18 14:52:21 +0200

    /

--- app/issues/list/issues-table.tsx
+++ app/issues/list/issues-table.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import NextLink from 'next/link'
 import { AiOutlineArrowUp } from "react-icons/ai";
 import IssueStatusBadge from "@/app/components/issue-status-bandage";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 export interface IssueQuery {
   status: Status;
   orderBy: keyof Issue;
@@ -15,13 +17,15 @@ interface Props {
   issue: Issue[];
 
 }
-function IssuesTable({ searchParams, issue }: Props) {
+async function IssuesTable({ searchParams, issue }: Props) {
+  const session = await getServerSession(options);
 
   return (
     <Table.Root variant="surface" className="">
       <Table.Header>
         <Table.Row>
-          {columns.map((column) => (
+          {columns.map((column) => 
+      
             <Table.ColumnHeaderCell justify={'center'}  key={column.label} className={column.className}>
                 <NextLink href={{
                     query: {
@@ -33,16 +37,16 @@ function IssuesTable({ searchParams, issue }: Props) {
                     <AiOutlineArrowUp className="inline"/>
                 )}
             </Table.ColumnHeaderCell>
-          ))}
+          )}
         </Table.Row>
       </Table.Header>
       <Table.Body>
         {issue.map(issue => (
             <Table.Row key={issue.id}>
                 <Table.Cell justify={'center'} >
-                    <Link href={`/issues/${issue.id}`}>
+                    
                         {issue.title}
-                    </Link>
+                    
                     <div className="block md:hidden">
                         <IssueStatusBadge status={issue.status}/>
                     </div>
@@ -53,9 +57,10 @@ function IssuesTable({ searchParams, issue }: Props) {
               <Table.Cell justify={'center'} className="hidden md:table-cell">
               {issue.createdAt instanceof Date ? issue.createdAt.toDateString(): 'N/A'}
               </Table.Cell>
-              {/* <Table.Cell justify={'center'}  className="hidden md:table-cell ">
-                <IssuesStatusToggle issueProps={issue}/>
-              </Table.Cell> */}
+              {session?.user.role === "ADMIN" &&  <Table.Cell justify={'center'}  className="hidden md:table-cell"> 
+              <Link href={`/issues/${issue.id}`} className="self-center bg-red-500 w-20 align-middle p-2 text-white block rounded-lg">Edit</Link>
+              </Table.Cell>}
+             
             </Table.Row>
         ))}
       </Table.Body>
@@ -80,11 +85,11 @@ const columns: {
     value: "createdAt",
     className: "hidden md:table-cell",
   },
-  // {
-  //   label:"State",
-  //   value:'status',
-  //   className: "hidden md:table-cell",
-  // }
+  {
+    label:"State",
+    value:'status',
+    className: `hidden md:table-cell `,
+  }
 ];
 export const columnsNames = columns.map(columns => columns.value)
 export default IssuesTable;
